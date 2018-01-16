4f329a5aec32e9100af571885645340e2ce974d6
Nicolas Zin <nicolas.zin@gmail.com>
2018-01-16 03:11:59 -0500

    tablewidget is working

--- tablewidget.go
+++ tablewidget.go
@@ -1,6 +1,10 @@
 package sws
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const HEADER_HEIGHT = 25
 
 // TableData is an interface use to provide the Model (MVC)
 // aka the table datas
@@ -17,9 +21,11 @@ type TableData interface {
 
 type TableWidget struct {
 	CoreWidget
-	tabledata TableData
-	vertical  *ScrollbarWidget
-	yoffset   int32
+	tabledata       TableData
+	vertical        *ScrollbarWidget
+	headertextcolor sdl.Color
+	textcolor       sdl.Color
+	yoffset         int32
 }
 
 // NewTableWidget create a table widget, you need to provide a Model i.e. a TableData object
@@ -27,22 +33,24 @@ type TableWidget struct {
 func NewTableWidget(w, h int32, tabledata TableData) *TableWidget {
 	corewidget := NewCoreWidget(w, h)
 	widget := &TableWidget{
-		CoreWidget: *corewidget,
-		tabledata:  tabledata,
-		vertical:   NewScrollbarWidget(15, 20, false),
-		yoffset:    0,
+		CoreWidget:      *corewidget,
+		tabledata:       tabledata,
+		vertical:        NewScrollbarWidget(15, 20, false),
+		yoffset:         0,
+		textcolor:       sdl.Color{0, 0, 0, 255},
+		headertextcolor: sdl.Color{0, 0, 0, 255},
 	}
 
-	widget.vertical.Move(w-15, 30)
+	widget.vertical.Move(w-15, HEADER_HEIGHT)
 	widget.vertical.SetMinimum(0)
 	max := int32(0)
-	if h < 25*tabledata.GetNbRows()+30 {
-		max = 25*tabledata.GetNbRows() + 30 - h
+	if h < 25*tabledata.GetNbRows()+HEADER_HEIGHT {
+		max = 25*tabledata.GetNbRows() + HEADER_HEIGHT - h
 		widget.AddChild(widget.vertical)
 	}
 	widget.vertical.SetMaximum(max)
-	widget.vertical.SetCallbackValueChanged(func() {
-		widget.yoffset = widget.vertical.Currentposition
+	widget.vertical.SetCallback(func(currentposition int32) {
+		widget.yoffset = currentposition
 		widget.PostUpdate()
 	})
 
@@ -57,26 +65,37 @@ func NewTableWidget(w, h int32, tabledata TableData) *TableWidget {
 	return widget
 }
 
+func (self *TableWidget) SetHeaderTextColor(color sdl.Color) {
+	self.headertextcolor = color
+	self.PostUpdate()
+}
+
+func (self *TableWidget) SetTextColor(color sdl.Color) {
+	self.textcolor = color
+	self.PostUpdate()
+}
+
 func (self *TableWidget) Resize(width, height int32) {
 	self.CoreWidget.Resize(width, height)
 
 	// sanity
-	if height < 30 {
-		height = 30
+	if height < HEADER_HEIGHT {
+		height = HEADER_HEIGHT
 	}
 
-	self.vertical.Resize(15, height-30)
-	if height < 25*self.tabledata.GetNbRows()+30 {
-		self.CoreWidget.AddChild(self.vertical)
-		self.vertical.SetMaximum(25*self.tabledata.GetNbRows() + 30 - self.Height())
+	self.vertical.Resize(15, height-HEADER_HEIGHT)
+	if height < 25*self.tabledata.GetNbRows()+HEADER_HEIGHT {
+		self.vertical.Move(width-15, HEADER_HEIGHT)
+		self.AddChild(self.vertical)
+		self.vertical.SetMaximum(25*self.tabledata.GetNbRows() + HEADER_HEIGHT - self.Height())
 	} else {
-		self.CoreWidget.RemoveChild(self.vertical)
+		self.RemoveChild(self.vertical)
 	}
 	self.PostUpdate()
 }
 
-func (self *TableWidget) renderText(x, y, width, height int32, label string) {
-	text, err := self.Font().RenderUTF8Blended(label, sdl.Color{0x0, 0x0, 0x0, 0xff})
+func (self *TableWidget) renderText(x, y, width, height int32, label string, textcolor sdl.Color) {
+	text, err := self.Font().RenderUTF8Blended(label, textcolor)
 	if err != nil {
 		return
 	}
@@ -119,28 +138,37 @@ func (self *TableWidget) Repaint() {
 		xoffset := int32(0)
 		for i := int32(0); i < nbcolumns; i++ {
 			label := self.tabledata.GetCell(i, y)
-			self.renderText(xoffset, y*25+30-self.yoffset, columnSize[i], 25, label)
+			self.renderText(xoffset, y*25+HEADER_HEIGHT-self.yoffset, columnSize[i], 25, label, self.textcolor)
 			xoffset += columnSize[i]
 		}
 	}
 
 	// headers
-	self.FillRect(0, 0, self.width, 30, 0xffdddddd)
+	self.FillRect(0, 0, self.width, HEADER_HEIGHT, self.bgColor)
 	xoffset := int32(0)
 	for i := int32(0); i < nbcolumns; i++ {
 		label, size := self.tabledata.GetHeader(i)
-		self.renderText(xoffset, 0, size, 30, label)
+		self.renderText(xoffset, 0, size, HEADER_HEIGHT, label, self.headertextcolor)
+
+		//bezel
+		self.SetDrawColorHex(0xffffffff)
+		self.DrawLine(xoffset, 0, xoffset+size-1, 0)
+		self.DrawLine(xoffset, 0, xoffset, HEADER_HEIGHT-1)
+		self.SetDrawColor(50, 50, 50, 255)
+		self.DrawLine(xoffset+size-1, 1, xoffset+size-1, HEADER_HEIGHT-1)
+		self.DrawLine(xoffset+1, HEADER_HEIGHT-1, xoffset+size-1, HEADER_HEIGHT-1)
+
 		xoffset += size
 	}
 
-	// do we show the scrollbar
-	if self.Height() < 25*self.tabledata.GetNbRows()+30 {
-		if self.vertical.IsDirty() {
-			self.vertical.Repaint()
+	for _, child := range self.children {
+		// adjust the clipping to the current child
+		if child.IsDirty() {
+			child.Repaint()
 		}
-		rectSrc := sdl.Rect{0, 0, self.vertical.Width(), self.vertical.Height()}
-		rectDst := sdl.Rect{self.Width() - 1 - self.vertical.Width(), 30, self.vertical.Width(), self.vertical.Height()}
-		self.vertical.Surface().Blit(&rectSrc, self.Surface(), &rectDst)
+		rectSrc := sdl.Rect{0, 0, child.Width(), child.Height()}
+		rectDst := sdl.Rect{child.X(), child.Y(), child.Width(), child.Height()}
+		child.Surface().Blit(&rectSrc, self.Surface(), &rectDst)
 	}
-
+	self.dirty = false
 }
